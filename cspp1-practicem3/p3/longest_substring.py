ea43da2cc6d200970d240fe2f129bd06a77d40a4
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-02 19:41:48 -0700

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-practicem3/p3/longest_substring.py
+++ cspp1-practicem3/p3/longest_substring.py
@@ -1,15 +1,15 @@
 "'#enter a string'"
-S =  input()
+S = input()
 SUB = S[0]
 LEN = ' '
-i=1
-for i in range(1,len(S)):
+i = 1
+for i in range(1, len(S)):
     if SUB[-1] <= S[i]:
         SUB = SUB+S[i]
     else:
-        if len(SUB)>len(LEN):
-            LEN=SUB
-        SUB=S[i]
-    if len(SUB)>len(LEN):
-        LEN=SUB
+        if len(SUB) > len(LEN):
+            LEN = SUB
+        SUB = S[i]
+    if len(SUB) > len(LEN):
+        LEN = SUB
 print(LEN)
