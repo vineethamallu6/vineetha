227f8fcc3d199e8d275063075ad2f8a699cdf615
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-03 19:00:16 -0700

    " -> 2 of 4 passed. pylint: 6.67/10 "

--- cspp1-precticem5/p1/perfect_cube.py
+++ cspp1-precticem5/p1/perfect_cube.py
@@ -1,11 +1,11 @@
 "'#enter the input'"
 CUBE = int(input())
-for I in range (abs(CUBE)+1):
+for I in range (abs(CUBE) + 1):
     if I ** 3 >= abs(CUBE):
         break
 if I ** 3 != abs(CUBE):
     print(str(CUBE) + ' is not a perfect number')
 else:
     if CUBE < 0:
-        i -= 1
+        I -= 1
     print(str(CUBE)+ ' is a perfect cube')
