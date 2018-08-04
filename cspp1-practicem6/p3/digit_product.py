5af5e957800cb7e251f11c91f0452b7d840b4fc4
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-04 11:05:42 -0700

    " -> 6 of 7 passed. pylint: 8.89/10 "

--- cspp1-practicem6/p3/digit_product.py
+++ cspp1-practicem6/p3/digit_product.py
@@ -1,8 +1,11 @@
 "'input number'"
 NUM = int(input())
 MUL = 1
-while NUM > 0:
-    TEMP = NUM%10
-    MUL = MUL*TEMP
-    NUM = NUM//10
-print(MUL)
+if NUM == 0:
+    print('0')
+else:         
+    while NUM > 0:
+        TEMP = NUM%10
+        MUL = MUL*TEMP
+        NUM = NUM//10
+    print(MUL)
