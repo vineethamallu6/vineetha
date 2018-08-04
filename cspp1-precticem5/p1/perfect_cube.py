9a151bb568492dcd651eb599793dc06ea921c019
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-03 19:03:06 -0700

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1-precticem5/p1/perfect_cube.py
+++ cspp1-precticem5/p1/perfect_cube.py
@@ -5,7 +5,7 @@ for I in range(abs(CUBE) + 1):
     if I ** 3 >= abs(CUBE):
         break
 if I ** 3 != abs(CUBE):
-    print(str(CUBE) + ' is not a perfect number')
+    print(str(CUBE) + ' is not a perfect cube')
 else:
     if CUBE < 0:
         I -= 1
