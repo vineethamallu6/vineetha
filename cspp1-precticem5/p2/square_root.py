86d77d98af12472cac838f9fc477189d80a93f46
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-03 19:04:49 -0700

    " -> 3 of 3 passed. pylint: 3.33/10 "

---
+++ cspp1-precticem5/p2/square_root.py
@@ -0,0 +1,13 @@
+"'enter the input'"
+SQ=int(input())
+EPS=0.01
+I=0.0
+INCREMENT=0.1
+while abs(I**2 - SQ) >= EPS:
+    I += INCREMENT
+    
+if abs(I**2 - SQ) >= EPS:
+    print('failed')
+else:
+    print(I)
+        
