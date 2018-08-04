965f9ed1ba7cbb8955670b38a82db30231f223f0
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-03 19:12:53 -0700

    " -> 0 of 3 passed. pylint: 5.45/10 "

---
+++ cspp1-precticem5/p3/square_root_bisection.py
@@ -0,0 +1,13 @@
+"'enter the input'"
+SQ=int(input())
+EPS=0.01
+LOW=1.0
+HIGH=SQ
+RES=(HIGH+LOW)/2.0
+while abs(RES**2 - SQ) >= EPS:
+    if RES**2 < SQ:
+        LOW = RES
+    else:
+        HIGH = RES
+    RES = (HIGH+LOW)/2.0
+print(RES)
