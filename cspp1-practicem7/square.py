008548e9e9412e9b5ef5e656b4b6088323114925
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-06 13:00:50 -0700

    she

---
+++ cspp1-practicem7/square.py
@@ -0,0 +1,20 @@
+'"# input a value"'
+# Exercise: square
+# Write a Python function, square, that takes in one number and returns the square of that number.
+# This function takes in one number and returns one number.
+def square(x_int):
+    '''
+    x: int or float.
+    '''
+    return x_int**2
+def main():
+    "'#input a value'"
+    data = input()
+    data = float(data)
+    temp = str(data).split('.')
+    if temp[1] == '0':
+        print(square(int(float(str(data)))))
+    else:
+        print(square(data))
+if __name__ == "__main__":
+    main()
