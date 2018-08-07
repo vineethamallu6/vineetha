00b006b2a1ab17503dd6793215a76d75b297ae74
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-06 18:17:09 -0700

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1-practicem7/Functions - Assignment-1/assignment1.py
+++ cspp1-practicem7/Functions - Assignment-1/assignment1.py
@@ -26,23 +26,23 @@
 
 
 def payingDebtOffInAYear(balance, annual_interest_rate, monthly_payment_rate):
-        month = 1
-        while month <= 12:
-            monthly_interest_rate = (annual_interest_rate)/12.0
-            minimum_monthly_payment = (monthly_payment_rate)*(balance)
-            monthly_unpaid_balance = (balance)-(minimum_monthly_payment)
-            updated_balance = (monthly_unpaid_balance) + (monthly_interest_rate*monthly_unpaid_balance)
-            balance = updated_balance
-            month += 1
-        return round(updated_balance,2)
+    month = 1
+    while month <= 12:
+        monthly_interest_rate = (annual_interest_rate)/12.0
+        minimum_monthly_payment = (monthly_payment_rate)*(balance)
+        monthly_unpaid_balance = (balance)-(minimum_monthly_payment)
+        updated_balance = (monthly_unpaid_balance) + (monthly_interest_rate*monthly_unpaid_balance
+        balance = updated_balance
+        month += 1
+    return round(updated_balance,2)
             
 	
 
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	print("Remaining balance: "+str(payingDebtOffInAYear(data[0],data[1],data[2])))
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print("Remaining balance: "+str(payingDebtOffInAYear(data[0],data[1],data[2])))
 
 if __name__ == "__main__":
 	main()
