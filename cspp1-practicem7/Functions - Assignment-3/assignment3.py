52eaf1bba65acca9ac3fa835ad41a6afc7187c06
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-06 19:57:58 -0700

    " -> 8 of 10 passed. pylint: 9.29/10 "

--- cspp1-practicem7/Functions - Assignment-3/assignment3.py
+++ cspp1-practicem7/Functions - Assignment-3/assignment3.py
@@ -10,12 +10,12 @@ def paying_debt_offina_year(bal, annual_interest_rate):
     eps = 0.0001
     guess = (monthly_payment_lower_bound + monthly_payment_upper_bound)/2
     while True:
-        month=1
+        month = 1
         while month <= 12:
             new_balance = new_balance - guess
             new_balance = new_balance + (monthly_interest_rate * new_balance)
             month += 1
-        if new_balance> 0 and new_balance > eps:
+        if new_balance > 0 and new_balance > eps:
             monthly_payment_lower_bound = guess
             new_balance = bal
         elif new_balance < 0 and new_balance < -eps:
