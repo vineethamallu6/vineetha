41a5400012366f1cc16576d6be1fd39c29e332d4
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-07 17:53:10 -0700

    " -> 10 of 10 passed. pylint: 7.78/10 "

--- cspp1-practicem8/p1/assignment1.py
+++ cspp1-practicem8/p1/assignment1.py
@@ -6,7 +6,7 @@ def factorial(num):
     '''
     if num == 0 or num == 1:
         return 1
-    return num*factorial(num-1)
+    return num * factorial(num-1)
 def main():
     '''
     main function
