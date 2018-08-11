9e155cf021f11cafb1d9b29ae9f10a784752b75e
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-10 19:13:45 -0700

    t

---
+++ cspp1-practicem11/p3/assignment3.py
@@ -0,0 +1,51 @@
+# Assignment-3
+'''
+At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
+
+Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
+
+Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+'''
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+def isValidWord(word, hand, wordList):
+    """
+    Returns True if word is in the wordList and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+
+    Does not mutate hand or wordList.
+   
+    word: string
+    hand: dictionary (string -> int)
+    wordList: list of lowercase strings
+    """
+    string=''
+    if word in wordList:
+        for i in word:
+            if i in hand:
+                string=string+i
+    if string==word:
+        return "true"
+    return "false"
+           
+    
+
+
+def main():
+	word=input()
+	n=int(input())
+	adict={}
+	for i in range(n):
+		data=input()
+		l=data.split()
+		adict[l[0]]=int(l[1])
+	l2=input().split()
+	print(isValidWord(word,adict,l2))
+		
+
+
+if __name__== "__main__":
+	main()
