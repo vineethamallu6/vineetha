6e1c9f687048d5821aa597d392041a451962ac6a
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-14 18:05:45 -0700

    " -> 3 of 3 passed. pylint: 9.32/10 "

--- cspp1-practicem14/CodeCampPoker/poker.py
+++ cspp1-practicem14/CodeCampPoker/poker.py
@@ -84,8 +84,7 @@ def hand_rank(hand):
         return 2
     elif is_straight(hand):
         return 1
-    else:
-        return 0
+    return 0
 def poker(hands):
     '''
         This function is completed for you. Read it to learn the code.
