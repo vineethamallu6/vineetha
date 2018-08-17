9d53e15fdca69463e6a649749cfd503cd19007de
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-17 14:54:40 -0700

    " -> 2 of 3 passed. pylint: 9.73/10 "

--- cspp1-practicem16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practicem16/CodeCampDocumentDistance/document_distance.py
@@ -14,7 +14,7 @@ def similarity(dict1, dict2):
     for word in l_3:
         if word not in load_stopwords(FILE).keys():
             if word not in '!@#$%^&*()_+-.,=0123456789:' and "'":
-                dicti[word] = (list1.count(word), list2.count(word))
+                dicti[word] = (dict1.count(word), dict2.count(word))
     num = 0
     sum1 = 0
     sum2 = 0
