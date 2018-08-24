491bd4fe37ee2ac60ec96b8d7e8e02e75e406426
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-23 18:33:28 -0700

    " -> 4 of 8 passed. pylint: 3.57/10 "

--- cspp1-practicem19/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practicem19/CodeCampMatrixOperations/matrix_operations.py
@@ -8,9 +8,9 @@ def mult_matrix(mat1, mat2):
     '''
     if len(mat1[0])==len(mat2):
         result = [[0 for row in range(len(mat1))] for col in range(len(mat2[0]))]
-        for row in enumerate(len(mat1)):
-            for col in enumerate(len(mat2[0])):
-                for k_1 in enumerate(len(mat2)):
+        for row in range (len(mat1)):
+            for col in range (len(mat2[0])):
+                for k_1 in range (len(mat2)):
                     result[row][col] += mat1[row][k_1] * mat2[k_1][col]
         return result
     else:
@@ -46,9 +46,9 @@ def read_matrix(row,col):
     matrix = []
     for i in range(row):
         i+=1
-        input1 = input()
+        input1 = input().split(' ')
         if len(input1)==col:
-            matrix.append(list(map(int, input1.rstrip().split())))
+            matrix.append(list(map(int, input1)))
         else:
             print("Error:Invalid input for the matrix")
             return None
@@ -60,12 +60,14 @@ def read_matrix(row,col):
 
 def main():
     ''' read matrix''' 
-    rows,cols=map(int,input().split(','))
-    matrix1 = read_matrix(rows,cols)
+    
+    row,col=map(int,input().split(','))
+    matrix1 = read_matrix(row,col)
     if matrix1 is None:
         exit(0)
-    rows,cols=map(int,input().split(','))
-    matrix2 = read_matrix(rows,cols)
+    
+    row,col=map(int,input().split(','))
+    matrix2 = read_matrix(row,col)
     if matrix2 is None:
         exit(0)
     print(add_matrix(matrix1, matrix2))
