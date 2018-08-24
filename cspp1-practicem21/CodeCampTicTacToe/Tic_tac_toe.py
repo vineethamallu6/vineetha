be05c4dddf55507281b23ad9f9a2f4abffaa49ee
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-24 11:37:39 -0700

    " -> 12 of 14 passed. pylint: 6.00/10 "

--- cspp1-practicem21/CodeCampTicTacToe/Tic_tac_toe.py
+++ cspp1-practicem21/CodeCampTicTacToe/Tic_tac_toe.py
@@ -4,17 +4,17 @@ def Tic_tac_toe(matrix):
     for i in matrix:
         if i[0] == i[1] == i[2]:
             win.append(i[0])
-        for j in range(0, 3):
-            if matrix[0][j] == matrix[1][j] == matrix[2][j]:
-                win.append(matrix[0][j])
-            if matrix[0][0] == matrix[1][1] == matrix[2][2]:
-                win.append(matrix[0][0])
-            if matrix[2][0] == matrix[1][1] == matrix[0][2]:
-                win.append(matrix[0][2])
+    for j in range(0, 3):
+        if matrix[0][j] == matrix[1][j] == matrix[2][j]:
+            win.append(matrix[0][j])
+        if matrix[0][0] == matrix[1][1] == matrix[2][2]:
+            win.append(matrix[0][0])
+        if matrix[2][0] == matrix[1][1] == matrix[0][2]:
+            win.append(matrix[0][2])
     if len(win) == 0:
         print('draw')
         return None
-    if len(win) == 1:
+    elif len(win) == 1:
         if win[0] == 'x' or win[0] == 'o':
             print(win[0])
         else:
