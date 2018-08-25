b3075ae86513c10c72b2e243f221a01469427bc1
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-25 11:06:16 -0700

    " -> 2 of 3 passed. pylint: 8.00/10 "

--- cspp1-practicem22/assignment2/clean_input.py
+++ cspp1-practicem22/assignment2/clean_input.py
@@ -5,7 +5,7 @@ alphabets in both upper and lower case and numbers.
 import re
 def clean_string(string):
     '''cleaning words'''
-    reg = re.compile('[^!@#$%^&*()_+]')
+    reg = re.compile('[!@#$%^&*()_+]')
     string = reg.sub('',string)
     return string
 
