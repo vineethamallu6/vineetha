dc2b9f72947f921090395f6b75b50689238348dc
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-25 13:41:13 -0700

    " -> 0 of 4 passed. pylint: 6.00/10 "

--- cspp1-practicem22/assignment3/tokenize.py
+++ cspp1-practicem22/assignment3/tokenize.py
@@ -4,14 +4,14 @@ each word
 '''
 
 def tokenize(string):
-    dict1={}
-    word1=[]
+    dict1 = {}
+    word1 = []
     string=string.split(' ')
     for line in string:
         word1.append(line)
     for word in word1:
         if word not in dict1.keys():
-            dict1[word]=((i, j.count(j)) for i,j in enumerate(word1) )
+            dict1[word] = ((i, j.count(j)) for i,j in enumerate(word1) )
     return dict1
 
 
