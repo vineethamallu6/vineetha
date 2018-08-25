f8c90e31c61102475e2f4e75af156a12c0e75854
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-25 10:53:37 -0700

    " -> 2 of 2 passed. pylint: 4.00/10 "

--- cspp1-practicem15/gen_primes.py
+++ cspp1-practicem15/gen_primes.py
@@ -12,21 +12,14 @@ def genPrimes():
                break
        else:
            prime = True
-n = int(input())
-primenum = genPrimes()
-for i in range(n):
-   print(primenum.__next__())
 
 def main():
-    data=input()
-    l=data.split()
-    a=int(l[0])
-    b=int(l[1])
-    primeGenerator = genPrimes()
-    for i in range(a):
-        p=primeGenerator.next()
-        if(i%b==0):
-            print(p)
+   n = int(input())
+   primenum = genPrimes()
+   for i in range(n):
+       print(primenum.__next__())
+
+    
     
 if __name__== "__main__":
     main()
---
+++ cspp1-practicem22/assignment1/read_input.py
@@ -0,0 +1,15 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    string=""
+    n=int(input())
+    for i in range(n):
+        i+=1
+        string+=input()
+        string += '\n'
+    print(string)
+
+if __name__ == '__main__':
+    main()
